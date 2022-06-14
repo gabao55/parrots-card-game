a70dbd2b6e0846016fa2285446950150ec40bd39
gabao55 <gabriel.s.rosin@gmail.com>
2022-06-14 08:45:34 -0300

    Renders the cards according to inserted number of cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-let numberOfCards
+let numberOfCards;
+let numberOfInsertedCards = 0;
+let cardNode = document.createElement("div.card");
+cardNode.classList.add("card")
+cardNode.innerHTML = '<img src="assets/img/front.png" alt="" />';
 
-function startGame() {
+function receiveNumberOfCards() {
     numberOfCards = Number(prompt("Com quantas cartas quer jogar (de 4 a 14)?"));
     if (numberOfCards < 4) {
         alert("O número mínimo de cartas é 4!");
@@ -15,5 +19,15 @@ function startGame() {
         startGame();
     }
 }
+
+function startGame() {
+    while (numberOfInsertedCards < numberOfCards) {
+        document.querySelector(".cards").appendChild(cardNode.cloneNode(true));
+        numberOfInsertedCards++;
+        startGame();
+    }
+}
+
+receiveNumberOfCards();
 
 startGame();
