c47e27a70d4a687275fa4aa378b8b310ae1aeb27
gabao55 <gabriel.s.rosin@gmail.com>
2022-06-14 17:33:37 -0300

    Implements the functionality of flipping cards (no 3D effects and no sleep time)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let numberOfCards;
 let i;
 let allCards = document.querySelectorAll(".card");
+let frontImage;
+let backImage;
+let firstSelected = null;
+let secondSelected = null;
+let numberOfMoves = 0;
 
 function receiveNumberOfCards() {
     numberOfCards = Number(prompt("Com quantas cartas quer jogar (de 4 a 14 e par)?"));
@@ -25,9 +30,46 @@ function receiveNumberOfCards() {
 
 function startGame() {
     for (i = 0; i < numberOfCards; i++) {
-        console.log(allCards[i])
         allCards[i].classList.remove("display-none")
     }
+}
+
+function showCard(element) {
+    frontImage = element.querySelector('[data-identifier="front-face"]');
+    backImage = element.querySelector('[data-identifier="back-face"]');
+
+    if (frontImage.classList.contains("display-none") === true) {
+        frontImage.classList.toggle("display-none");
+        backImage.classList.toggle("display-none");
+        numberOfMoves++;
+    }
+    else {
+        return false;
+    }
+
+    if (firstSelected == null) {
+        firstSelected = frontImage;
+    }
+    else {
+        secondSelected = frontImage;
+    }
+
+    if (firstSelected != null && secondSelected != null) {
+        if (firstSelected.getAttribute("src") != secondSelected.getAttribute("src")) {
+            // TODO: Make the code sleep to show the second card
+            hideCards();
+        }
+
+        firstSelected = null;
+        secondSelected = null;
+    }
+}
+
+function hideCards() {
+    firstSelected.classList.toggle("display-none");
+    firstSelected.parentNode.querySelector('[data-identifier="back-face"]').classList.toggle("display-none");
+    secondSelected.classList.toggle("display-none");
+    secondSelected.parentNode.querySelector('[data-identifier="back-face"]').classList.toggle("display-none");
 }
 
 receiveNumberOfCards();
