75814f4d96647178887e90ab0156a422a1d4b7b6
gabao55 <gabriel.s.rosin@gmail.com>
2022-06-14 08:30:23 -0300

    Implements alerts for invalid inputs for number of cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-let numberOfCards = Number(prompt("Com quantas cartas quer jogar?"));
+let numberOfCards
+
+function startGame() {
+    numberOfCards = Number(prompt("Com quantas cartas quer jogar (de 4 a 14)?"));
+    if (numberOfCards < 4) {
+        alert("O número mínimo de cartas é 4!");
+        startGame();
+    }
+    else if (numberOfCards > 14) {
+        alert("O número máximo de cartas é 14!");
+        startGame();
+    }
+    else if (numberOfCards%2 != 0) {
+        alert("Você deve inserir um número par para jogar!");
+        startGame();
+    }
+}
+
+startGame();
